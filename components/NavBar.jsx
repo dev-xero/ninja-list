6384b8c652fa04e14894fa435d422895a084b3c5
CodeNinja-tech <elo48858@gmail.com>
2022-10-10 20:00:09 +0100

    Modified Custom Logo

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const NavBar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>Ninja List</h1>
+        <Image src='/icon.png' alt='logo' width={100} height={32}></Image>
       </div>
       <Link href='/'>
         <a>Home</a>
