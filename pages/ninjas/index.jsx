c41ff4b8a3ee7e5836e3335cbde0235fbdc0a21e
CodeNinja-tech <elo48858@gmail.com>
2022-10-10 20:47:39 +0100

    Updated CSS File

--- pages/ninjas/index.jsx
+++ pages/ninjas/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 
+import styles from '../../styles/Ninjas.module.css'
+
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users')
   const data = await res.json()
-  
-  return { 
-    props: { ninjas: data }
+
+  return {
+    props: { ninjas: data },
   }
 }
 
@@ -22,7 +24,9 @@ const Ninjas = ({ ninjas }) => {
       <h1>All Ninjas</h1>
       {ninjas.map(ninja => (
         <div key={ninja.id}>
-          <a><h3>{ ninja.name }</h3></a>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
         </div>
       ))}
     </>
