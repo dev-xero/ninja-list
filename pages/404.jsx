e296b8ad5e9549a98bc97816016b5061eef781d3
CodeNinja-tech <elo48858@gmail.com>
2022-10-10 19:47:44 +0100

    Used the NEXT Router

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const NotFound = () => {
+  const router = useRouter()
+
+  const redirectUser = () => {
+    setTimeout(() => {
+      router.push('/')
+    }, 3000)
+  }
+
+  useEffect(() => {
+    redirectUser()
+  })
+
   return (
     <div className="not-found">
       <h1>404</h1>
       <h2>That page could not be found</h2>
       <p>Go back to the <Link href='/'><a>home page</a></Link> </p>
+      <p className="redirect-msg">You&apos;ll be redirected</p>
     </div>
   )
 }
